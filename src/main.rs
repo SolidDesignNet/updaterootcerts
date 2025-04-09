af565504dc75eb4ba8ba36248a965995e988815f
Joe Batt <Joe@SolidDesign.net>
2025-04-09 17:37:49 -0400

    clean up and testing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 #![windows_subsystem = "windows"]
 
 use anyhow::Result;
-#[cfg(feature = "gui")]
-use fltk::dialog::message;
+
 use reqwest::blocking::get;
 
 fn main() -> Result<()> {
@@ -20,9 +19,12 @@ fn main() -> Result<()> {
     for site in sites {
         eprint!("Updating {site}");
         let res = get(site).map(|_| "OK").unwrap_or("");
-        eprintln!("{res:?}");
+        eprintln!("  {res}");
     }
     #[cfg(feature = "gui")]
-    message(200, 200, "Done updating Root Certificates");
+    {
+        fltk_theme::WidgetTheme::new(fltk_theme::ThemeType::Metro).apply();
+        fltk::dialog::message(200, 200, "Done updating Root Certificates");
+    }
     Ok(())
 }
